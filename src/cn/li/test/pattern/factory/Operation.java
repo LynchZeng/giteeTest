549b91125de261280426a05ea115f9c6c4beecb9
budongyingyu <760865548@qq.com>
2017-11-10 14:41:02 +0800

    Initial commit

---
+++ src/cn/li/test/pattern/factory/Operation.java
@@ -0,0 +1,11 @@
+package cn.li.test.pattern.factory;
+
+public abstract class Operation {
+	public double numberA;
+	public double numberB;
+	/**
+	 * ?????????
+	 */
+	public abstract double getResult();
+	
+}
